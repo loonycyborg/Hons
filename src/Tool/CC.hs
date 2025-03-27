df0e3b42b1ca34cb319e2bc34bf1e571462ade8b
loonycyborg <loonycyborg@gmail.com>
2025-03-27 05:56:01 +0300

    Added type signature to CC.compile and CC.link

--- src/Tool/CC.hs
+++ src/Tool/CC.hs
@@ -5,6 +5,8 @@ import Builder
 import CmdLine
 import Taskmaster
 import Environment
+import Node
+import DepGraph
 
 data Flag where
     Compile :: Flag
@@ -14,11 +16,13 @@ instance Argument Flag where
     toCmdLine Compile = [encodeArg "-c"]
     toCmdLine (Output a) = encodeArg "-o" : toCmdLine a
 
+compile :: (ConstructionVariable (LookupType "cc.com" vars), Argument (LookupType "cc.com" vars)) => Node -> Node -> RuleSet vars
 compile target source = command target source do
     env <- getenv
     let cc = eLookup @"cc.com" env
     fmap success $ liftIO $ spawnCmdPrint $ Cmd cc :$ Compile :$ Output target :$ source
 
+link :: (ConstructionVariable (LookupType "cc.linkcom" vars), Argument (LookupType "cc.linkcom" vars), Argument s, NodeList s) => Node -> s -> RuleSet vars
 link target sources = command target sources do
     env <- getenv
     let ld = eLookup @"cc.linkcom" env
