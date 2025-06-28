7b3d6082d7ab31d12253f0aa118e7e6370337549
loonycyborg <loonycyborg@gmail.com>
2025-06-28 19:11:25 +0300

    Use own implementation of topological sort

--- src/DepGraph.hs
+++ src/DepGraph.hs
@@ -3,11 +3,11 @@ module DepGraph where
 import Node ( Node )
 import Taskmaster (Task(Task))
 import Algebra.Graph.AdjacencyMap
-    ( connect, overlay, overlays, vertex, vertices, AdjacencyMap, induce, empty )
-import Algebra.Graph.AdjacencyMap.Algorithm (reachable, topSort)
+    ( connect, overlay, overlays, vertex, vertices, AdjacencyMap, induce, empty, postSet )
 import qualified Data.Set as Set
 import qualified Data.HashMap.Strict as HM
 import qualified Data.List.NonEmpty as L
+import Data.Semigroup (sconcat)
 
 type DepGraph = AdjacencyMap Node
 type TaskList vars = HM.HashMap Node (Task vars)
@@ -27,10 +27,26 @@ instance Monoid (RuleSet vars) where
     mempty = RuleSet HM.empty empty
 
 buildOrder :: RuleSet vars -> Node -> L.NonEmpty (Node, Maybe (Task vars))
-buildOrder ruleset goal = order where
-    componentVertices = Set.fromList (reachable ruleset.graph goal)
-    component = induce (`Set.member` componentVertices) ruleset.graph
-    order = L.reverse $ case topSort component of
-        Left cycle -> error ("Dependency cycle detected: " ++ show cycle)
-        Right l    -> L.map mkItem (L.fromList l)
+buildOrder ruleset goal = L.reverse $ L.map mkItem $ topologicalSort ruleset.graph goal where
     mkItem n = (n, HM.lookup n ruleset.tasks)
+
+data VertexSearchState = Discovered | Finished deriving (Show, Eq)
+type SearchState a n = HM.HashMap n (VertexSearchState, a)
+
+depthFirstFold :: (a1 -> Node -> a1) -> (a1 -> [a2] -> [a1] -> [a1] -> a2) -> DepGraph -> Node -> a1 -> a2
+depthFirstFold discover_func finish_func graph vertex a1 =
+    fst $ go HM.empty vertex (discover_func a1 vertex) where
+        go search vertex a1 = finish_vertex $ foldr check_edge (HM.insert vertex (Discovered, a1) search, [], [], []) (postSet vertex graph)
+            where
+            check_edge target_vertex (search, tree_edges, front_cross_edges, back_edges) = case HM.lookup target_vertex search of
+                Nothing               -> (search',a2:tree_edges,    front_cross_edges,    back_edges) where
+                    (a2, search') = go search target_vertex (discover_func a1 target_vertex)
+                Just (Discovered, a1) -> (search,    tree_edges,    front_cross_edges, a1:back_edges)
+                Just (Finished, a1)   -> (search,    tree_edges, a1:front_cross_edges,    back_edges)
+            finish_vertex (search, tree_edges, front_cross_edges, back_edges) =
+                (finish_func a1 tree_edges front_cross_edges back_edges, HM.adjust (\(Discovered, x) -> (Finished, x)) vertex search)
+
+topologicalSort :: DepGraph -> Node -> L.NonEmpty Node
+topologicalSort graph vertex = depthFirstFold (\l n -> n:l) topS graph vertex [] where
+    topS (x:_) xs _ []         = sconcat $ L.singleton x L.:| xs
+    topS xs    _  _ ((b:_):bs) = error $ "Dependency cycle detected: " ++ (show $ b : (reverse $ b : takeWhile (/=b) xs))
