f1ef55fecbd3d0094195e684b67fdb1778d41f29
loonycyborg <loonycyborg@gmail.com>
2025-11-04 19:03:14 +0300

    Use TH to generate accessors for construction variables

--- src/Tool/CC.hs
+++ src/Tool/CC.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BlockArguments, DataKinds, TemplateHaskell #-}
+{-# LANGUAGE BlockArguments, DataKinds, TemplateHaskell, ImplicitParams #-}
 {-# OPTIONS_GHC -Werror=incomplete-patterns #-}
 module Tool.CC where
 
@@ -26,7 +26,7 @@ instance Argument Flag where
     toCmdLine (Output a) = encodeArg "-o" : toCmdLine a
 
 compile :: (UseEnv ToolVars vars) => Node -> Node -> RuleSet vars
-compile = osCommand $ Cmd (subst ("cc" :. "com")) :$ Compile :$ Output substT :$ substS
+compile = osCommand $ Cmd com :$ Compile :$ Output substT :$ substS
 
 link :: (UseEnv ToolVars vars, Argument s, NodeList s) => Node -> s -> RuleSet vars
-link = osCommand $ Cmd (subst ("cc" :. "linkcom")) :$ Output substT :$ substS
+link = osCommand $ Cmd linkcom :$ Output substT :$ substS
--- src/ToolTH.hs
+++ src/ToolTH.hs
@@ -1,8 +1,32 @@
 module ToolTH where
 import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+import Data.Char (toLower)
 
 genToolVars :: Q [Dec]
 genToolVars = do
     Just name <- lookupValueName "toolEnv"
     AppT (ConT _) toolVars <- reifyType name
-    pure [TySynD (mkName "ToolVars") [] toolVars]
+    let vars = var_list toolVars
+    Module _ (ModName m) <- thisModule
+    let toolset = toolset_name m
+    pure $ TySynD (mkName "ToolVars") [] toolVars : concatMap (mk_accessor toolset) vars
+    where
+        var_list (AppT (AppT _ (AppT (AppT _ (AppT _ (LitT (StrTyLit var)))) _)) rest) = var : var_list rest
+        var_list (SigT PromotedNilT (AppT ListT StarT)) = []
+        toolset_name = map toLower . reverse . takeWhile (/='.') . reverse
+        mk_accessor toolset var = [
+            SigD acc_name (
+                ForallT [] [
+                    AppT (AppT (ConT $ mkName "UseEnv") (ConT $ mkName "ToolVars")) (VarT vars),
+                    ImplicitParamT "e" (AppT (ConT $ mkName "Environment") (VarT vars)),
+                    AppT (
+                        AppT EqualityT (AppT (AppT (ConT $ mkName "LookupType") (AppT (AppT (PromotedT $ mkName ":.") (LitT (StrTyLit toolset))) (LitT (StrTyLit var)))) (VarT vars))
+                        ) (VarT a)
+                    ] (VarT a)
+                ),
+            ValD (VarP acc_name) (NormalB (AppE (UnboundVarE $ mkName "subst") (InfixE (Just (LitE (StringL toolset))) (ConE $ mkName ":.") (Just (LitE (StringL var)))))) []
+            ] where
+                acc_name = mkName var
+                vars = mkName "vars"
+                a = mkName "a"
