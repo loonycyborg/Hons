4ba661c12c2e7f666ec00e90ec50959d7db06894
loonycyborg <loonycyborg@gmail.com>
2026-05-01 12:16:11 +0300

    Use OverloadedLists for Honstruct files to avoid having special syntax for nonempty lists

--- app/Main.hs
+++ app/Main.hs
@@ -64,7 +64,7 @@ main = defaultErrorHandler defaultFatalMessager defaultFlushOut do
         dflags <- liftIO $ act dflags
             { backend   = interpreterBackend
             , ghcLink   = LinkInMemory
-            , extensionFlags = dflags.extensionFlags <> fromList [ OverloadedRecordDot, QuasiQuotes, DataKinds, BlockArguments, OverloadedStrings, ExtendedDefaultRules ] `difference` fromList [FieldSelectors]
+            , extensionFlags = dflags.extensionFlags <> fromList [ OverloadedRecordDot, QuasiQuotes, DataKinds, BlockArguments, OverloadedStrings, OverloadedLists, ExtendedDefaultRules ] `difference` fromList [FieldSelectors]
             , packageEnv = env }
         setSessionDynFlags dflags
         let script_file = invoc_settings.file
