2f2607ec2067da63d83111c30e95964db811a9ea
loonycyborg <loonycyborg@gmail.com>
2026-06-11 23:01:10 +0300

    Add support for serializing OsStrings and Nodes

--- src/Node.hs
+++ src/Node.hs
@@ -1,8 +1,13 @@
-{-# LANGUAGE TypeFamilies, BlockArguments, RequiredTypeArguments, TemplateHaskellQuotes #-}
+{-# LANGUAGE TypeFamilies, BlockArguments, RequiredTypeArguments, TemplateHaskellQuotes, DerivingVia, DeriveAnyClass #-}
 
 module Node where
 import System.OsPath
 import System.Directory.OsPath
+import System.OsString (coercionToPlatformTypes)
+import System.OsString.Internal.Types
+    ( OsString(OsString),
+      PosixString(PosixString, getPosixString),
+      WindowsString(getWindowsString) )
 import Algebra.Graph.AdjacencyMap
 import GHC.IO (unsafePerformIO)
 import Data.Hashable ( Hashable(hashWithSalt) )
@@ -11,17 +16,23 @@ import Language.Haskell.TH.Syntax ( Lift(lift, liftTyped) )
 import Data.List.NonEmpty (NonEmpty ((:|)), fromList)
 import Data.Foldable1 (Foldable1 (foldMap1))
 import qualified Data.List.NonEmpty as NE
+import qualified Data.HashMap.Strict as HM
 import Control.Monad (unless)
 import Control.Monad.IO.Class
 import Control.DeepSeq ( force )
-import Text.Read hiding (lift)
+import Text.Read hiding (lift, get)
 import Control.Applicative (Alternative((<|>)))
 import Type.Reflection
+import Type.Reflection.Unsafe (someTypeRepFingerprint)
+import GHC.Fingerprint ( Fingerprint )
+import GHC.Generics (Generic, Generically(..))
+import Data.Binary
 import Data.Kind
+import Data.Type.Coercion (coerceWith)
 
 import Value
 
-data ValueTyHolder = forall a . (Value a) => ValueTyHolder (TypeRep a)
+data ValueTyHolder = forall a . (Value a, Binary a, Typeable a) => ValueTyHolder (TypeRep a)
 deriving instance Show ValueTyHolder
 instance Eq ValueTyHolder where
     (ValueTyHolder a1) == (ValueTyHolder a2) = SomeTypeRep a1 == SomeTypeRep a2
@@ -31,17 +42,39 @@ instance Hashable ValueTyHolder where
     hashWithSalt salt (ValueTyHolder a) = hashWithSalt salt a
 instance Lift ValueTyHolder where
     liftTyped (ValueTyHolder _) = [||ValueTyHolder (typeRep @())||]
+instance Binary ValueTyHolder where
+    put (ValueTyHolder a) = put (someTypeRepFingerprint (SomeTypeRep a))
+    get = do
+        f <- get @Fingerprint
+        maybe (fail "Unknown TypeRep fingerprint") return $ HM.lookup f fingerprints
+        where
+            fingerprints = HM.insert (someTypeRepFingerprint (SomeTypeRep (typeRep @()))) mkUnitTyHolder HM.empty
 
-mkTyHolder :: forall (ty :: Type) -> (Typeable ty, Value ty) => ValueTyHolder
+mkTyHolder :: forall (ty :: Type) -> (Typeable ty, Value ty, Binary ty) => ValueTyHolder
 mkTyHolder ty = ValueTyHolder (typeRep @ty)
 
 mkUnitTyHolder :: ValueTyHolder
 mkUnitTyHolder = ValueTyHolder (typeRep @())
 
+instance Binary OsString where
+    put str = case coercionToPlatformTypes of
+        Left (_, c)  -> do
+            put (0 :: Word8)
+            put $ getWindowsString $ coerceWith c str
+        Right (_, c) -> do
+            put (1 :: Word8)
+            put $ getPosixString   $ coerceWith c str
+    get = do
+        tag <- getWord8
+        case tag of
+            1 -> OsString . PosixString <$> get
+            _ -> fail "Failed to deserialize OsString"
+
 data Node where
     ValueNode :: { name :: String, ty :: ValueTyHolder } -> Node
     FsNode :: { path :: OsPath } -> Node
-    deriving (Eq, Ord, Lift)
+    deriving stock (Eq, Ord, Lift, Generic)
+    deriving anyclass Binary
 
 valueTypeSuffix :: forall (a :: Type). TypeRep a -> String
 valueTypeSuffix t = if SomeTypeRep t /= SomeTypeRep (typeRep @()) then "!" <> show t else ""
