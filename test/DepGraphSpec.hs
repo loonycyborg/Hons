1036e64c179fb2d96ee3c1d02483879027d471e5
loonycyborg <loonycyborg@gmail.com>
2026-08-30 01:45:34 +0300

    Generate random graphs straight as AdjacencyMaps

--- test/DepGraphSpec.hs
+++ test/DepGraphSpec.hs
@@ -2,12 +2,10 @@ module DepGraphSpec (spec) where
 
 import Test.Syd
 import Test.QuickCheck
-import Algebra.Graph
 import qualified Algebra.Graph.AdjacencyMap as AM
 import qualified Algebra.Graph.AdjacencyMap.Algorithm as AMA
 import qualified Data.Set as Set
 import qualified Data.List.NonEmpty as NE
-import Algebra.Graph.ToGraph (ToGraph(toAdjacencyMap))
 import Data.Bool
 import Data.Either
 import Control.Exception
@@ -15,15 +13,15 @@ import Control.Exception
 import DepGraph
 import Node
 
-instance (Arbitrary a, Ord a) => Arbitrary (Graph a) where
-    arbitrary = overlay <$> (vertices <$> arbitrary) <*> (edges <$> arbitrary)
+instance (Arbitrary a, Ord a) => Arbitrary (AM.AdjacencyMap a) where
+    arbitrary = AM.overlay <$> (AM.vertices <$> arbitrary) <*> (AM.edges <$> arbitrary)
 
 spec :: Spec
 spec = do
     describe "DepthFirstFold algorithm" $ do
         it "topsorts correctly" $
-            property $ \(g :: Graph Int) ->
-                let depgraph :: DepGraph = AM.gmap (mkValue . show) $ toAdjacencyMap g
+            property $ \(g :: AM.AdjacencyMap Int) ->
+                let depgraph :: DepGraph = AM.gmap (mkValue . show) g
                     edges = AM.edgeList depgraph
                     verts = AM.vertexSet depgraph
                     subgraph_verts = bool (AMA.reachable depgraph (minimum verts)) [] $ Set.null verts
